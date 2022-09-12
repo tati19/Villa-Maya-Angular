37cf3c83139cf613358c3cc74966386f6d18c7a1
Christophe Caphane <ccaphane@gmail.com>
2022-09-12 20:00:18 +0400

    Html and CSS draft design

--- VillaMaya/src/app/app.module.ts
+++ VillaMaya/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/partials/header/header.component';
+import { AboutComponent } from './components/about/about.component';
+import { BookingComponent } from './components/booking/booking.component';
+import { ReviewsComponent } from './components/reviews/reviews.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    BookingComponent,
+    ReviewsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
